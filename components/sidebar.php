04a71e32f6f18da1263e512b34be8416d2bd9ac9
Satria Yusuf Saputra <satriasys2@gmail.com>
2024-12-31 18:26:01 +0700

    penambahan edit profile dokter

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,3 +1,9 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <!-- <a href="index3.html" class="brand-link">
@@ -14,7 +20,7 @@
                 <img src="assets/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block"><?php echo $username ?></a>
+            <a href="#" class="d-block"><?php echo $_SESSION['username']; ?></a>
             </div>
         </div>
 
@@ -84,6 +90,12 @@
                             <a href="dashboard_dokter.php" class="nav-link">
                                 <i class="fas fa-solid fas fa-th nav-icon"></i>
                                 <p>Dashboard <span class="right badge badge-success">Dokter</span></p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="dokterProfile.php" class="nav-link">
+                                <i class="fas fa-solid fa-user-nurse nav-icon"></i>
+                                <p>Profile <span class="right badge badge-success">Dokter</span></p>
                             </a>
                         </li>
                         <li class="nav-item">
